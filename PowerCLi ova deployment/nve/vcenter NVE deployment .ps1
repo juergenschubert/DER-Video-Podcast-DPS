1e904ec52271f91da0dc90f5adab363e7917769e
juergenschubert <juergen.schubert@web.de>
2020-02-27 14:18:23 +0100

    latest and greates

--- PowerCLi ova deployment/nve/vcenter NVE deployment .ps1
+++ PowerCLi ova deployment/nve/vcenter NVE deployment .ps1
@@ -1,13 +1,11 @@
 ﻿$vCenter = "vc.vlab.local"
-$nveName = "NVE-19-2"
-$vcenterUser = "ppdm@vsphere.local"
-$vcenterpw = "Password123!"
+$aveName = "NVE-19-2"
+
+Connect-VIServer -protocol https -Server $vCenter -User ppdm@vsphere.local -Password Password123!
 
 #########
 # vCenter connection done
 #########
-Connect-VIServer -protocol https -Server $vCenter -User $vcenterUser -Password $vcenterpw
-
 #NetWorker
 $ovfFile = "S:\networker\NW19.2 GA\NVE-19.2.0.112.ova"
 
@@ -17,18 +15,18 @@ $ovfFile = "S:\networker\NW19.2 GA\NVE-19.2.0.112.ova"
 
 $ovfConfig = Get-OvfConfiguration $ovfFile
 ###########
-# Enter the nve configuration parameters
+# Enter the AVE configuration parameters
 ###########
 #show me the  configuration option you get from the ova
 $ovfconfig.ToHashTable() | ft –autosize
 
 ##########
-#IF you hnve no idea what variables to use and what they are used for a NetWorker deployment #########
-#$ovfConfig
+#IF you have no Idea what variables to use and what they are used for a NetWorker deployment #########
+$ovfConfig
 #$ovfConfig.NetworkMapping.VM_Network
 #$ovfConfig.NetworkMapping.VM_Network.Value = "VM Network"
-#$ovfConfig.vami
-#$ovfConfig.vami.NetWorker_Virtual_Edition
+$ovfConfig.vami
+$ovfConfig.vami.NetWorker_Virtual_Edition
 #$ovfConfig.vami.NetWorker_Virtual_Edition.ipv4
 #$ovfConfig.vami.NetWorker_Virtual_Edition.ipv6
 #$ovfConfig.vami.NetWorker_Virtual_Edition.gatewayv4
@@ -40,7 +38,7 @@ $ovfconfig.ToHashTable() | ft –autosize
 #$ovfConfig.vami.NetWorker_Virtual_Edition.NVEtimezone
 #$ovfConfig.vami.NetWorker_Virtual_Edition.DDIP
 #$ovfConfig.vami.NetWorker_Virtual_Edition.DDBoostUseExistingUser
-#$ovfConfig.vami.NetWorker_Virtual_Edition.DDBoostUsername
+#$ovfConfig.vami.NetWorker_Virtual_Edition.DDBoostUsername    
 #$ovfConfig.vami.NetWorker_Virtual_Edition.vCenterFQDN
 $ovfConfig.vami.NetWorker_Virtual_Edition.ipv4.value = "192.168.1.33"
 #$ovfConfig.vami.NetWorker_Virtual_Edition.ipv6.value = ""
@@ -54,7 +52,7 @@ $ovfConfig.vami.NetWorker_Virtual_Edition.NVEtimezone.value = ""
 $ovfConfig.vami.NetWorker_Virtual_Edition.DDIP.value = "192.168.1.90"
 $ovfConfig.vami.NetWorker_Virtual_Edition.DDBoostUseExistingUser.value ="Yes"
 $ovfConfig.vami.NetWorker_Virtual_Edition.DDBoostUsername.value = "ddboost"
-$ovfConfig.vami.NetWorker_Virtual_Edition.vCenterFQDN.value = "vc.vlab.local"
+$ovfConfig.vami.NetWorker_Virtual_Edition.vCenterFQDN.value = "vc.vlab.local"                           
 $ovfConfig.vami.NetWorker_Virtual_Edition.vCenterUsername.value = "ppdm@vsphere.local"
 $ovfConfig.NetworkMapping.VM_Network.Value = "VM Network"
 $ovfConfig.vami.NetWorker_Virtual_Edition.NVEtimezone.value = 'Europe/Berlin'
@@ -63,7 +61,7 @@ $ovfConfig.vami.NetWorker_Virtual_Edition.NVEtimezone.value = 'Europe/Berlin'
 #show me the  configuration option you get from the ova
 $ovfconfig.ToHashTable() | ft –autosize
 ##########
-# Select: vmhost, datastore, network to configure the NVE
+# Select: vmhost, datastore, network to configure the AVE
 $VMHost = Get-Cluster "Cluster" | Get-VMHost | Where-Object { $_.Name -like "esx1.vlab.local*"}
 $dataStore = $VMHost | Get-Datastore  | Where-Object { $_.Name -like "nfs_datastore*"}
 $Network = Get-VirtualPortGroup -Name "VM Network" -VMHost $VMHost
@@ -75,9 +73,9 @@ $Network = Get-VirtualPortGroup -Name "VM Network" -VMHost $VMHost
 #Get-VM
 #####################
 
-Write-Output "Waiting for nve VM deployment..."
+Write-Output "Waiting for NVE VM deployment..."
 $startTime = get-date
-$avamarVM = Import-VApp -Source $ovfFile -OvfConfiguration $ovfConfig -Name "$nveName" -VMHost $VMHost -Datastore $dataStore -DiskStorageFormat thin
+$avamarVM = Import-VApp -Source $ovfFile -OvfConfiguration $ovfConfig -Name "$aveName" -VMHost $VMHost -Datastore $dataStore -DiskStorageFormat thin
 $endTime = get-date
 
 $DeploymentTime = $endTime - $startTime
@@ -88,16 +86,16 @@ Write-Output " ***** `nNVE deployment lasted for: $DeploymentTime`nDisconnecting
 ##########
 #Get-VM
 #
-Get-VM -Name $nveName | Get-NetworkAdapter
-##Get-VM -Name “nve-20” | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName “OpaqueNetworkName”
+Get-VM -Name $aveName | Get-NetworkAdapter
+##Get-VM -Name “ave-20” | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName “OpaqueNetworkName”
 ##########
 ###vm status
 ##########
-Get-VMGuest -vm $nveName
+Get-VMGuest -vm $aveName
 ##########
 ### start/stop vms
 ##########
-start-vm $nveName
-# shutdown-vmguest $nveName
+start-vm $aveName
+# shutdown-vmguest $aveName
 
 Disconnect-VIServer -Server $vCenter -confirm:$false
